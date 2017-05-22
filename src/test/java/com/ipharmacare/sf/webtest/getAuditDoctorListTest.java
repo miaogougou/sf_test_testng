2e80cac9b272ff63b7111d8c578d3701e7b19565
daijunjun <daijj@ipharmacare.net>
2017-05-22 09:52:40 +0800

    add new testcases

---
+++ src/test/java/com/ipharmacare/sf/webtest/getAuditDoctorListTest.java
@@ -0,0 +1,46 @@
+package com.ipharmacare.sf.webtest;
+
+
+import java.util.HashMap;
+import java.util.Random;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+
+import com.ipharmacare.sf.webtest.common.*;
+import com.ipharmacare.sf.webtest.common.ResponseEntity.StatusInfo;
+
+
+
+public class getAuditDoctorListTest {
+    
+    public String Code=null,Data=null,Msg=null,Result=null;
+    getAuditDoctorList req=new getAuditDoctorList();
+    
+    @Test(groups = { "BaseCase1"})
+    public void getAuditDoctorList_Succ() throws Exception{
+        resultCheck(StatusInfo.REQUEST_SUCCESS); 
+    }
+     
+    public void resultCheck( StatusInfo statusInfo ) throws Exception{
+        Result=req.getHttpRespone();  
+        Reporter.log("请求地址: "+req.geturl());
+        Reporter.log("返回结果: "+Result);
+        System.out.println("请求地址: "+req.geturl());
+        System.out.println("返回结果: "+Result);
+        Code=Common.getJsonValue(Result, "code");
+        Data=Common.getJsonValue(Result, "data");
+        Msg=Common.getJsonValue(Result, "message");
+        if(null != statusInfo ) {
+        	Assert.assertEquals(Code,statusInfo.getCode()); 
+            Assert.assertEquals(Msg,statusInfo.getMessage());
+        }
+    }
+}
