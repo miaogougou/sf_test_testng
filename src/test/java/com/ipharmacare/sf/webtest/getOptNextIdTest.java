eae88f913d352dab4075bfc0752e9ed9c6da9d0a
daijunjun <daijj@ipharmacare.net>
2017-06-01 16:43:02 +0800

    add newtestcases 20170601

---
+++ src/test/java/com/ipharmacare/sf/webtest/getOptNextIdTest.java
@@ -0,0 +1,49 @@
+package com.ipharmacare.sf.webtest;
+
+
+import java.io.IOException;
+import java.util.Random;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+import org.testng.annotations.Parameters;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+
+import com.ipharmacare.sf.webtest.common.*;
+import com.ipharmacare.sf.webtest.common.ResponseEntity.StatusInfo;
+
+
+
+public class getOptNextIdTest {
+    
+    public String Code=null,Data=null,OptRecipeInfo=null,Result=null,optRecipeId=null;
+    getOptNextId req=new getOptNextId();
+    
+    
+    @Parameters({"optRecipeId"})
+	@Test(groups = { "BaseCase1"})
+    public void getOptNextId_Succ(String optRecipeId) throws Exception{
+        resultCheck(optRecipeId,StatusInfo.REQUEST_SUCCESS); 
+    }
+     
+    public void resultCheck(String optRecipeId, StatusInfo statusInfo ) throws Exception{
+        Result=req.getHttpRespone(optRecipeId);  
+        Reporter.log("请求地址: "+req.geturl());
+        Reporter.log("返回结果: "+Result);
+        System.out.println("请求地址: "+req.geturl());
+        System.out.println("返回结果: "+Result);
+        Code=Common.getJsonValue(Result, "code");
+        Data=Common.getJsonValue(Result, "data");
+        OptRecipeInfo=Common.getJsonValue(Result, "message");
+        if(null != statusInfo ) {
+        	Assert.assertEquals(Code,statusInfo.getCode()); 
+            Assert.assertEquals(OptRecipeInfo,statusInfo.getMessage());
+        }
+    }
+}
